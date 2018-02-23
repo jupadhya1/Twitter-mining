99b026247c335ba645cf152b7fca4b0e5cd1f8c3
jupadhya1 <jitendraupadhyay10@gmail.com>
2018-02-23 23:47:59 +0800

    Delete eval-twitter-mining.sh

--- eval-twitter-mining.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python eval.py \
---pos_file=../sentiment140/test.pos.txt \
---neg_file=../sentiment140/test.neg.txt \
---vocab_file=./word2vec/vocab.txt \
---checkpoint_dir=./checkpoints
